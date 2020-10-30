f96add65c2151918006d8e40ee4bcc133864cff1
Yago Cunha <yago.cunha123@gmail.com>
2020-10-30 14:34:26 -0300

    Configurando typeorm typescript e criando a tabela tool

---
+++ api/src/shared/infra/typeorm/migrations/1604077990169-CreateTool.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTool1604077990169 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tools',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'link',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'tags',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tools');
+  }
+}
