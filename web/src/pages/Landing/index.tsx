463c37e92882983cb6126ec76b46836f4c1423e0
Yago Cunha <yago.cunha123@gmail.com>
2020-11-05 15:22:31 -0300

    Listando ferramentas na Landing

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTools } from '../../hooks/tools';
 
 import Header from '../../components/Header';
+import ToolItem from '../../components/ToolItem';
+import Modal from '../../components/Modal';
 
 import { Container } from './styles';
 
-const Landing: React.FC = () => (
-  <Container>
-    <Header />
-  </Container>
-);
+const Landing: React.FC = () => {
+  const { loadTools, tools } = useTools();
+
+  useEffect(() => {
+    loadTools();
+  }, [loadTools]);
+
+  return (
+    <Container>
+      <Modal />
+      <Header />
+      <ul>
+        {
+          tools.map((tool) => (
+            <ToolItem
+              key={tool.id}
+              title={tool.title}
+              link={tool.link}
+              description={tool.description}
+              tags={tool.tags}
+            />
+          ))
+        }
+      </ul>
+    </Container>
+  );
+};
 
 export default Landing;
--- web/src/pages/Landing/styles.ts
+++ web/src/pages/Landing/styles.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    list-style-type:none;
+  }
+
+  li {
+    align-self: center;
+    margin: 20px 0px;
+  }
+`;
